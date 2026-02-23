9cc5f1971844d0ce6ea728fbce853cfc92f1e56c
Friedman Global <friedmangloballlc@gmail.com>
2026-02-23 09:27:36 +0000

    File changes

--- src/pages/SubscriptionManagement.jsx
+++ src/pages/SubscriptionManagement.jsx
@@ -13,7 +13,7 @@ import BillingHistory from "@/components/subscription/BillingHistory";
 import { TALENT_PLANS, BRAND_PLANS, AGENCY_PLANS } from "@/components/subscription/SubscriptionPlans";
 import { useSearchParams } from "react-router-dom";
 
-const stripePromise = loadStripe(Deno.env.get("STRIPE_PUBLISHABLE_KEY") || "");
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY || "");
 
 export default function SubscriptionManagement() {
   const [searchParams] = useSearchParams();
